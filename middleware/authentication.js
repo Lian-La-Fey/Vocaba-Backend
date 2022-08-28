c510df4c3c33b6ab69e8a87e4dd6fe2eb82cc188
Lian-La-Fey <alamedrin@yandex.com>
2022-08-28 18:03:46 +0300

    Some of the comments deleted..

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,20 +1,5 @@
 import jwt from "jsonwebtoken";
 
-// const auth = (req, res, next) => {
-//     const token = req.header('x-auth-token')
-
-//     if(!token)
-//         return res.status(400).send({msg: "Access denied, no token provided." })
-
-//     jwt.verify(token, process.env.JWT_PRIVATE_KEY, (err, validToken) => {
-//         if(err)
-//             return res.status(400).send({ msg: "Invalid Token" })
-
-//         req.user = validToken;
-//         next();
-//     })
-// }
-
 const auth = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
